addef808d5fddc3fc019bd0b2630c3dc006cd77e
AntonMazur <Anton_Mazur@epam.com>
2018-05-14 01:52:37 +0300

    1) Refactoring 2) Pre final version

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ version := "0.1"
 
 scalaVersion := "2.12.6"
 
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+
 libraryDependencies ++= Seq(
   "org.scala-lang" % "scala-swing" % "2.10.5",
   "com.typesafe.akka" %% "akka-actor" % "2.5.12")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,109 @@
-import java.awt.Toolkit
-import java.io.File
+import java.awt.{Image, Toolkit}
 
-import javax.swing.{JFrame, JPanel, JScrollPane, JTextArea}
+import akka.actor.{Props, ActorRef, ActorSystem}
+import Properties._
 
-import scala.swing._
+import scala.collection.mutable.Set
+import javax.swing.{JTextArea, _}
+
+import scala.concurrent.Future
+import scala.util.{Random}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App{
-  val pathToResources = System.getProperty("user.dir") + "/src/main/resources/"
-  val mainWindow = new JFrame()
-  mainWindow.setLayout(null)
-  mainWindow.setTitle("Model STP")
-  mainWindow.setBounds(250, 100, 850, 500)
-  mainWindow.setIconImage(Toolkit.getDefaultToolkit.getImage(pathToResources + "scalaLogo.jpeg"))
-  mainWindow.add(buildTextArea(isEditable = true))
+//  runSTP(mockData())
 
+  def runSTP(adjMatr: Array[Array[Boolean]]): Array[Array[Boolean]] = {
+    val system = ActorSystem("System")
+    val nodes = Set[ActorRef]()
+    val nodeIDs = generateSetOfInts(adjMatr.length)
+    for (i <- 0 until adjMatr.length) {
+      var nbhrs = Seq[Int]()
+      for (j <- 0 until adjMatr(0).length if i != j && adjMatr(i)(j))
+        nbhrs = nbhrs :+ nodeIDs(j)
+      Future { nodes.add(system.actorOf(Props(Node(nodeIDs(i), nbhrs)))) }
+      Thread.sleep(2000)
+    }
 
-  def buildTextArea(x: Int = 200, y: Int = 100, width: Int = 300, height: Int = 200,
-                    wrapLines: Boolean = true, isEditable: Boolean = false): JScrollPane = {
-    val textArea = new JTextArea()
-    textArea.setLineWrap(wrapLines)
-    textArea.setEditable(isEditable)
-    val scPane = new JScrollPane(textArea)
-    scPane.setBounds(x, y, width, height)
-    scPane
+    Node.run(array2map(nodeIDs))
   }
 
-  mainWindow.setVisible(true)
+  def array2map(arr: Array[Int]): Map[Int, Int] = {
+    (for (i <- 0 until arr.length) yield (arr(i) -> i)).toMap
+  }
+
+  def buildUI(): JFrame = {
+    def buildTextArea(x: Int = 200, y: Int = 100, width: Int = 350, height: Int = 350,
+                      wrapLines: Boolean = true, isEditable: Boolean = false): (JScrollPane, JTextArea) = {
+      val textArea = new JTextArea()
+      textArea.setLineWrap(wrapLines)
+      textArea.setEditable(isEditable)
+      textArea.setFont(textArea.getFont().deriveFont(16f))
+      val scPane = new JScrollPane(textArea)
+      scPane.setBounds(x, y, width, height)
+      (scPane, textArea)
+    }
+
+    def loadImage(path: String): Image = Toolkit.getDefaultToolkit.getImage(pathToResources + path)
+
+    def buildButton(x: Int = 150, y: Int = 100, width: Int = 150, height: Int = 100,
+                    text: String = null, pathToIcon: String = null): JButton = {
+      val button = if (pathToIcon == null) new JButton(text) else new JButton(new ImageIcon(loadImage(pathToIcon)))
+      button.setBounds(x, y, width, height)
+      button
+    }
+
+    val mainWindow = new JFrame()
+    mainWindow.setLayout(null)
+    mainWindow.setTitle("Model STP")
+    mainWindow.setBounds(250, 100, 1050, 700)
+    mainWindow.setIconImage(loadImage("scalaLogo.jpeg"))
+    val (inputSP, inputTA) = buildTextArea(x = 30, y = 50, isEditable = true)
+    mainWindow.add(inputSP)
+    val (outSP, logTA) = buildTextArea(x = 400, y = 30, width = 600, height = 600)
+    mainWindow.add(outSP)
+    val startButton = buildButton(y = 500, pathToIcon = "play-button.jpg")
+    Node.setLogger(logTA)
+    startButton.addActionListener(event => {
+      val result = Future {
+        runSTP(readAdjMatrix(inputTA.getText split ("\n") map (_.split(" "))))
+      }
+      result foreach (result => {
+          val binRes: Array[Array[Int]] = result map (row => row map (bool => if (bool) 1 else 0))
+          Node.log(binRes.map(row => row.mkString(" ")).mkString("\n"))
+        });
+    });
+    mainWindow.add(startButton)
+    mainWindow
+  }
+
+  def readAdjMatrix(lines: Array[Array[String]]): Array[Array[Boolean]] = {
+    val n = lines.length
+    val adjMatr: Array[Array[Boolean]] = Array.ofDim(n, n)
+    for (i <- 0 until n; j <- 0 until n if (j != i)) {
+      adjMatr(i)(j) = if (lines(i)(j) == "1") true else false
+    }
+    adjMatr
+  }
+
+  def mockData(): Array[Array[Boolean]] ={
+    Array[Array[Boolean]](
+      Array(false, true, true, true, true),
+      Array(true, false, true, true, true),
+      Array(true, true, false, true, true),
+      Array(true, true, true, false, true),
+      Array(true, true, true, false, true))
+  }
+
+  def generateSetOfInts(size: Int): Array[Int] = {
+    val intSet = Set[Int]()
+    while (intSet.size != size)
+      intSet.add(Random.nextInt().abs % 1000)
+    intSet.toArray
+
+  }
+
+  buildUI().setVisible(true)
 }
 
 
